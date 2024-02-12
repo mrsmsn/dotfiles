653ce268236a7f47351456a2baae7e71ba49889b
morishima <morishima@morishimanoMacBook-Pro.local>
2024-02-12 18:04:32 +0900

    Update my dotfiles

---
+++ .wezterm.lua
@@ -0,0 +1,26 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.window_background_opacity = 0.70
+config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font("HackGen Console NF", {weight="Regular", stretch="Normal", style="Normal"})
+
+-- Fullscreen start
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():toggle_fullscreen()
+end)
+
+-- Shortcut keys
+local act = wezterm.action
+config.keys = {
+  {
+    key = 'f',
+    mods = 'CMD|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
+return config
