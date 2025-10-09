d985ccdf43ff5818e37901a5ded8f17a39db5ed5
mrsmsn <53550779+mrsmsn@users.noreply.github.com>
2025-10-09 10:10:48 +0900

    mod wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.window_background_opacity = 0.75
+config.window_background_opacity = 0.9
 config.hide_tab_bar_if_only_one_tab = true
 config.audible_bell = "Disabled"
 config.font = wezterm.font("HackGen Console NF", {weight="Regular", stretch="Normal", style="Normal"})
@@ -16,12 +16,23 @@ end)
 
 -- Shortcut keys
 local act = wezterm.action
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+config.keys = {
+  {
+    key = 'f',
+    mods = 'CTRL|SHIFT',
+    action = act.ToggleFullScreen,
+  },
+}
+else 
 config.keys = {
   {
     key = 'f',
     mods = 'CMD|CTRL',
-    action = wezterm.action.ToggleFullScreen,
+    action = act.ToggleFullScreen,
   },
 }
+end
+
 
 return config
