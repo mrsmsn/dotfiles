c69ac9814e8e77da7ac1c70019998c25b7d4309e
mrsmsn <53550779+mrsmsn@users.noreply.github.com>
2025-11-17 01:29:42 +0900

    add background.lua

---
+++ .config/wezterm/background.lua
@@ -0,0 +1,13 @@
+local background_image = "C:/Users/mosh4/Picture/usagi.jpg"
+
+return {
+	{
+		source = {
+			File = background_image,
+		},
+		opacity = 0.15,
+		hsb = {
+			brightness = 0.2,
+		},
+	},
+}
